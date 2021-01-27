4cd11cf9fcc25b497b25122d65ad879350583c6a
orrgorenn <orrgorenn@gmail.com>
2021-01-27 11:17:08 +0200

    Update auth

--- src/routes/buildings.js
+++ src/routes/buildings.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const { getBuildings, getBuilding, createBuilding, editBuilding, deleteBuilding } = require('../controllers/buildings');
-const { authUser } = require('../utils/auth');
-const { validateCreateBuilding, isReqValid } = require('../validators/building');
+const express = require("express");
+const {
+    getBuildings,
+    getBuilding,
+    createBuilding,
+    editBuilding,
+    deleteBuilding,
+} = require("../controllers/buildings");
+const { authUser } = require("../utils/auth");
+const {
+    validateCreateBuilding,
+    isReqValid,
+} = require("../validators/building");
 
 const router = express.Router();
 
-router.get('/', getBuildings);
-router.post('/', validateCreateBuilding, isReqValid, createBuilding);
-router.get('/:id', authUser, getBuilding);
-router.put('/:id', authUser, editBuilding);
-router.delete('/:id', authUser, deleteBuilding);
+router.get("/", getBuildings);
+router.post("/", validateCreateBuilding, isReqValid, createBuilding);
+router.get("/:id", getBuilding);
+router.put("/:id", authUser, editBuilding);
+router.delete("/:id", authUser, deleteBuilding);
 
 module.exports = router;
